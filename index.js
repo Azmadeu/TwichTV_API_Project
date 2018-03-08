f43218bda6fd5f573786ff37727a57bc4b1dda94
Azmadeu <doubleworkz@mail.ru>
2018-03-08 19:12:28 +0200

    add twich channels info

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-$.ajax({
-    url:'https://api.twitch.tv/kraken/streams/freecodecamp?callback=?',
-    dataType: 'jsonp',
-    crossDomain: true,
-    success: function (data) {
-        console.log(data);
+function channels() {
+    var channelsWhoRegularyStream = ["SilverName", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
+        "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+    for (var i = 0; i < channelsWhoRegularyStream.length; i++) {
+        $.ajax({
+            url: 'https://api.twitch.tv/kraken/channels/' + channelsWhoRegularyStream[i] + '',
+            headers: {
+                'Client-ID': 'zvjl7lpopsx54ajrpdfay8ma2l1vo5'
+            },
+            Accept: 'application/vnd.twitchtv.v5+json',
+            success: function (data) {
+                console.log(data);
+                var logoType = data.logo;
+                var displayName = data.display_name;
+                var channelsUrl = data.url;
+                $('.channels-list ul').append('<li>' +
+                    '<img src="' + logoType + '">' +
+                    +displayName +
+                    '</li>')
+            },
+            error: function (err) {
+
+            }
+        });
     }
-})
+}
+
+    window.onload = function () {
+        channels();
+};
+
